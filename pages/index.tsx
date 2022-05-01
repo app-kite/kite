c343493362ea0ab31e22390afa7f0e4c248d0f56
eden lane <edencore@gmail.com>
2022-05-02 01:27:00 +0300

    Added nextjs and styled components

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type {NextPage} from 'next'
+import styled from 'styled-components';
+
+const Home: NextPage = () => {
+  return (
+    <Main>Test</Main>
+  )
+}
+
+const Main = styled.main`
+  color: red;
+`;
+
+export default Home
