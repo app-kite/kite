947e3a5d035e7ae879956b26929af06e4211ab31
shue <aprosvirov093@gmail.com>
2022-05-05 00:39:40 +0400

    feat(ui): added storybook

---
+++ packages/ui/components/Text/Text.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Text } from "../Text";
+
+export default {
+  title: "ui/Text",
+  component: Text,
+} as ComponentMeta<typeof Text>;
+
+const PlaygroundContent: ComponentStory<typeof Text> = (args) => {
+  return <Text {...args} />;
+};
+
+export const Default = PlaygroundContent.bind({});
+
+Default.args = {
+  children: "Пример стори",
+};
