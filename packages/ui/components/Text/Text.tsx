fb1bd44ac9ba6b8c43a12f14fda47d108bca3e43
eden lane <edencore@gmail.com>
2022-05-07 10:24:25 +0300

    Added sizes to Text component

--- packages/ui/components/Text/Text.tsx
+++ packages/ui/components/Text/Text.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  size: 'sm' | 'md' | 'lg';
+}
 
 /**
  * Component for typography
  */
-export const Text = ({ children }) => {
-  return <span>{children}</span>;
+export const Text = ({
+  children,
+  ...props
+}) => {
+  return <Root {...props}>{children}</Root>;
 };
+
+const Root = styled.span<Props>`
+  font-size: ${p => {
+    switch (p.size) {
+      case 'sm':
+        return '12px';
+      case 'md':
+        return '14px';
+      case 'lg':
+        return '16px';
+    }
+  }};
+`;
