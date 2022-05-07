d0af9a27be6fb57980a297cb850a7dfd43a730a6
eden lane <edencore@gmail.com>
2022-05-07 10:25:12 +0300

    closes #10: Added Vote component

---
+++ packages/ui/components/Vote/Vote.stories.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
+import {Vote} from './Vote';
+
+export default {
+  title: 'ui/Vote',
+  component: Vote,
+} as ComponentMeta<typeof Vote>;
+
+const PlaygroundContent: ComponentStory<typeof Vote> = (args) => {
+  const [isActivated, setActivated] = useState(false);
+  const value = args.value;
+
+  const handleClick = (isActivated) => {
+    setActivated(isActivated);
+  }
+
+  return <Vote value={isActivated ? value + 1 : value} onChange={handleClick} isActivated={isActivated} />
+}
+
+export const Default = PlaygroundContent.bind({});
+
+Default.args = {
+  value: 381,
+}
---
+++ packages/ui/components/Vote/Vote.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Text} from '../Text';
+import ChevronUpIcon from '../../assets/icons/chevron-up.svg';
+
+type Props = {
+  value: number;
+  onChange(activated: boolean): void;
+  isActivated: boolean;
+}
+
+export const Vote = ({
+  value,
+  onChange,
+  isActivated,
+}) => {
+
+  const handleClick = () => {
+    onChange(!isActivated)
+  }
+
+  return (
+    <Root onClick={handleClick} isActivated={isActivated}>
+      <ChevronUpIcon />
+      <Text size='sm'>{value}</Text>
+    </Root>
+  )
+}
+
+const Root = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  width: 38px;
+  height: 58px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  background-color: #EDEDED;
+  font-weight: bold;
+  cursor: pointer;
+  color: ${p => p.isActivated ? p.theme.primaryColor : p.theme.textColor};
+  user-select: none;
+`;
