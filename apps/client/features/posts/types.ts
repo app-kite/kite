8bf9a9b5e78634618768732277b0a0d062d0bdb9
shue <aprosvirov093@gmail.com>
2022-05-09 23:34:22 +0400

    closes #28: API: List of posts

--- apps/client/features/posts/types.ts
+++ apps/client/features/posts/types.ts
@@ -1,7 +1,15 @@
 import { Descendant } from '@kite/ui';
 
+type CategoryPayload = {
+  id: number;
+  name: string;
+};
+
 export type Post = {
   id: string;
   title: string;
   text: Descendant[];
+  categoryId: number;
+  authorId: number;
+  category: CategoryPayload;
 };
