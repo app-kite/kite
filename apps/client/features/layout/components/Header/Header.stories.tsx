626290cfb01a44d2d5dd1d8df54219e9d1dfcf31
Maksim Aisin <maksim.aisin@mail.ru>
2022-06-01 00:30:38 +0300

    feat: header component

--- apps/client/features/layout/components/Header/Header.stories.tsx
+++ apps/client/features/layout/components/Header/Header.stories.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
 import { Header } from './Header';
+import styled from 'styled-components';
+import { SessionProvider } from 'next-auth/react';
 
 export default {
   title: 'Features/Layout/Components/Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
-const PlaygroundContent: ComponentStory<typeof Header> = () => {
-  return <Header />;
+const session = {
+  expires: '2022-02-02',
+  user: {
+    id: '1',
+    image: 'https://i.pravatar.cc/100',
+    name: 'Eiven Petrov',
+    email: 'test@mail.ru',
+  },
 };
 
-export const Default = PlaygroundContent.bind({});
+export const SignedIn = () => {
+  return (
+    <SessionProvider session={session}>
+      <Header />
+    </SessionProvider>
+  );
+};
+
+export const SignedOut = () => {
+  return (
+    <SessionProvider session={null}>
+      <Header />
+    </SessionProvider>
+  );
+};
