84d132d54d47334f60efa5498b646d1ab09fde61
eden lane <edencore@gmail.com>
2022-06-18 17:19:11 +0300

    Added component Select

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -3,5 +3,6 @@ export * from './components/Editor';
 export * from './components/Form';
 export * from './components/Input';
 export * from './components/Modal';
+export * from './components/Select';
 export * from './components/Text';
 export * from './components/Vote';
